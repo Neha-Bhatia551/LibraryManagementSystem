460e074716089abd9c57e3db924d9455a21dadf1
Zaeem Khan <hafizzaeemkhan@gmail.com>
2023-10-01 10:01:09 -0500

    Pushing data columns

--- src/main/java/edu/depaul/cdm/se452/fall2023group1/user/User.java
+++ src/main/java/edu/depaul/cdm/se452/fall2023group1/user/User.java
@@ -6,11 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import java.util.List;
 
@@ -24,11 +20,20 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name="user_id")
     private long userId;
+    @Column(name="first_name")
     private String firstName;
+    @Column(name="last_name")
     private String lastName;
+    @Column(name="email")
     @Email(regexp = ".+[@].+[\\.].+")
     private String email;
+
+    // Currently two roles - Librarian & Borrower
+    // TODO: Check if more roles are necessary
+    @Column(name="role")
     private String role;
     private List<String> borrowedBooks;
 }
