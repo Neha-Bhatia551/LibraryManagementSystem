02af500d54d5188e9ab7765861778dfda60d7493
Zaeem Khan <hafizzaeemkhan@gmail.com>
2023-10-01 17:10:08 -0500

    added crud operations

--- src/main/java/edu/depaul/cdm/se452/fall2023group1/user/UserController.java
+++ src/main/java/edu/depaul/cdm/se452/fall2023group1/user/UserController.java
@@ -2,9 +2,7 @@ package edu.depaul.cdm.se452.fall2023group1.user;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -12,11 +10,25 @@ import java.util.List;
 @RequestMapping("/users")
 @Log4j2
 public class UserController {
-//    @Autowired
-//    private UserRepository repository;
+    @Autowired
+    private UserRepository repository;
+
+    @PostMapping
+    public User addAUser(@RequestBody User user) {
+        log.info("adding a user");
+        return repository.save(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(User user) {
+        log.info("deleting user", user);
+        repository.delete(user);
+
+    }
 
     @GetMapping
-    public List<User> list() {
-        return null;
+    public List<User> getAllUsers() {
+        log.info("Getting all users");
+        return repository.findAll();
     }
 }
