e320faae410d12b4e6a95fa31c0824922e5d0777
Zaeem Khan <hafizzaeemkhan@gmail.com>
2023-10-01 10:17:15 -0500

    schema setup

--- src/main/java/edu/depaul/cdm/se452/fall2023group1/user/User.java
+++ src/main/java/edu/depaul/cdm/se452/fall2023group1/user/User.java
@@ -22,7 +22,7 @@ public class User {
     private long id;
 
     @Column(name="user_id")
-    private long userId;
+    private int userId;
     @Column(name="first_name")
     private String firstName;
     @Column(name="last_name")
