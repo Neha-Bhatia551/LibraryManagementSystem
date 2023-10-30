be2971bf0ebe1094ec9e2955acfb463738b85b1e
skv815 <sergei.maze@gmail.com>
2023-10-30 15:15:54 -0500

    service layer for reviews

--- src/main/java/edu/depaul/cdm/se452/fall2023group1/reviews/Review.java
+++ src/main/java/edu/depaul/cdm/se452/fall2023group1/reviews/Review.java
@@ -1,31 +1,36 @@
 package edu.depaul.cdm.se452.fall2023group1.reviews;
 
+import edu.depaul.cdm.se452.fall2023group1.books.Book;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.apache.logging.log4j.core.config.plugins.validation.constraints.Required;
 
 @Entity
-@Table(name = "review")
+@Table(name = "reviews")
 @Data
 @RequiredArgsConstructor
 @NoArgsConstructor(force = true)
 public class Review {
     @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id")
     @NonNull
-    private Integer reviewId;
+    private Long reviewId;
 
-    @Column(name = "author")
+    @Column(name = "user_id")
     @NonNull
-    private Integer userId;
+    private Long userId;
 
-    @Column(name = "isbn")
+    @Column(name = "book_id")
     @NonNull
-    private String isbn;
+    private Long bookId;
+
+    @ManyToOne
+    @JoinColumn(name = "isbn", referencedColumnName = "ISBN")
+    @NonNull
+    private Book book;
 
     @Column(name = "stars")
     @NonNull
--- src/main/java/edu/depaul/cdm/se452/fall2023group1/reviews/ReviewController.java
+++ src/main/java/edu/depaul/cdm/se452/fall2023group1/reviews/ReviewController.java
@@ -2,25 +2,75 @@ package edu.depaul.cdm.se452.fall2023group1.reviews;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+import java.util.List;
 
 @RestController
-@RequestMapping("/reviews")
+@RequestMapping("/api/reviews")
 @Log4j2
 public class ReviewController {
 
     @Autowired
-    private ReviewRepository reviews;
+    private ReviewService reviewService;
 
-    @GetMapping("/1")
-    public ResponseEntity<Optional<Review>> getReviewTest() {
-        return ResponseEntity.ok(reviews.findById(1));
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getReviewById(@PathVariable Long id) throws ReviewNotFoundException{
+        try {
+            Review review = reviewService.getReview(id);
+            return ResponseEntity.ok(review);
+        }
+        catch (ReviewNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<?> createReview(@PathVariable Long id, @RequestBody Review review) {
+        return ResponseEntity.ok().body(reviewService.saveOrUpdate(id, review));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateReview(@PathVariable Long id, @RequestBody Review review) {
+        return ResponseEntity.ok().body(reviewService.saveOrUpdate(id, review));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteReview(@PathVariable Long id) throws ReviewNotFoundException{
+        reviewService.delete(id);
+        // Todo: return ResponseEntity
+    }
+
+    @GetMapping("/book")
+    public ResponseEntity<?> getReviewsByBookId(@RequestParam() Long id, @RequestParam(required = false) Integer stars) throws ReviewNotFoundException {
+//        public ResponseEntity<?> getReviewsByBookId(@RequestParam() Long id) throws ReviewNotFoundException {
+        try {
+//            Integer stars = null;
+            if (stars == null) {
+                List<Review> reviews = reviewService.getReviewsByBook(id);
+                return ResponseEntity.ok(reviews);
+            }
+            else {
+                List<Review> reviews = reviewService.getReviewsByBookAndRating(id, stars);
+                return ResponseEntity.ok(reviews);
+            }
+        }
+        catch (ReviewNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/author")
+    public ResponseEntity<?> getReviewsByUserId(@RequestParam Long user) throws ReviewNotFoundException{
+        try {
+            List<Review> reviews = reviewService.getReviewsByUserId(user);
+            return ResponseEntity.ok(reviews);
+        }
+        catch (ReviewNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 
 }
