6b8bc41cf89b5ad4674b7af3737609a88083b7da
Neha-Bhatia551 <nbhatia4@depaul.edu>
2023-11-20 00:42:13 -0600

    Added a UI for handling book reservations

--- src/main/java/edu/depaul/cdm/se452/fall2023group1/bookreservations/BookReservationController.java
+++ src/main/java/edu/depaul/cdm/se452/fall2023group1/bookreservations/BookReservationController.java
@@ -1,12 +1,18 @@
 package edu.depaul.cdm.se452.fall2023group1.bookreservations;
 
+import edu.depaul.cdm.se452.fall2023group1.books.Book;
 import edu.depaul.cdm.se452.fall2023group1.books.BookService;
+import edu.depaul.cdm.se452.fall2023group1.user.User;
 import edu.depaul.cdm.se452.fall2023group1.user.UserRepository;
+import edu.depaul.cdm.se452.fall2023group1.user.UserService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/bookreservation")
@@ -21,12 +27,15 @@ public class BookReservationController {
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
     public String list(Model model, HttpSession session) {
         model.addAttribute("reservations", service.getAllReservations());
         model.addAttribute("books", bookService.list());
-        model.addAttribute("users", repository.findAll());
-        if (session.getAttribute("course") == null) {
+        model.addAttribute("users", userService.list());
+        if (session.getAttribute("reservation") == null) {
             model.addAttribute("reservation", new BookReservation());
             model.addAttribute("btnAddOrModifyLabel", "Reserve Book");
         } else {
@@ -36,18 +45,36 @@ public class BookReservationController {
         return "bookreservations/list";
     }
 
+    @GetMapping("/edit/{id}")
+    public String get(@PathVariable("id") Long id, Model model, HttpSession session) {
+        session.setAttribute("reservation", service.getReservationById(id));
+        return "redirect:/bookreservation";
+    }
+
     @PostMapping
     public String save(@ModelAttribute BookReservation reservation, HttpSession session) {
-//        if (course.getId() == 0)
-//            service.create(course);
-//        else {
-//            var editCourse = service.find(course.getId());
-//            editCourse.setDept(course.getDept());
-//            editCourse.setNum(course.getNum());
-//            editCourse.setDescription(course.getDescription());
-//            service.create(editCourse);
-//            session.setAttribute("course", null);
-//        }
+        if (reservation.getReservationId() == 0) {
+            java.sql.Date now = new java.sql.Date(new java.util.Date().getTime());
+            reservation.setBorrowDate(now);
+            reservation.setCheckedOut(true);
+            service.save(reservation);
+        } else {
+            var editReservation = service.getReservationById(reservation.getReservationId());
+            Optional<Book> book = bookService.findById(reservation.getBook().getBook_id());
+            Optional<User> user = repository.findById(reservation.getUser().getId());
+            editReservation.setBook(book.get());
+            editReservation.setUser(user.get());
+            editReservation.setReturnDate(reservation.getReturnDate());
+            editReservation.setType(reservation.getType());
+            service.save(editReservation);
+            session.setAttribute("reservation", null);
+        }
+        return "redirect:/bookreservation";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Long id, Model model) {
+        service.deleteReservation(id);
         return "redirect:/bookreservation";
     }
 }
