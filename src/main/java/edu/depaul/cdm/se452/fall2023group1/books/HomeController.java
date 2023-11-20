84b60b49f96b333763d5bfdf338d5c5ce67ae4cd
Neha-Bhatia551 <nbhatia4@depaul.edu>
2023-11-20 15:17:28 -0600

    fix ui issue

--- src/main/java/edu/depaul/cdm/se452/fall2023group1/books/HomeController.java
+++ src/main/java/edu/depaul/cdm/se452/fall2023group1/books/HomeController.java
@@ -2,14 +2,16 @@ package edu.depaul.cdm.se452.fall2023group1.books;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+@RequestMapping("/book")
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping
     public String index() {
-        return "index";
+        return  "books/list";
     }
 }
 
